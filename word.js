6f432d8e38e1868a950770ca0132554ddd3d3b3c
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-23 20:25:39 -0700

    connect letters to words, add display function to word

--- word.js
+++ word.js
@@ -1,17 +1,29 @@
 var Letter = require("./letter.js");
 
-var Word = function(word) {
-    this.word = word;
-    this.makeGuess = function(){
-        var temp = ""
-        for(var i = 0; i < this.word.length; i++){
+var Word = function(wordIn) {
+    this.addWord = function(wordInput) {
+        var wordTemp = [];
+        for(var i = 0; i < wordInput.length; i++){
+            wordTemp.push(new Letter(wordInput[i]));
+        }
+        return wordTemp;
+    }
+    this.word = this.addWord(wordIn);
+    this.makeGuess = function(num){
+        var temp = "";
+        for(var i = 0; i < num; i++){
             temp += "_";
         }
         return temp;
     };
-    this.guess = this.makeGuess(word);
+    this.guess = this.makeGuess(this.word.length);
+    this.display = function() {
+        var showThis = ""
+        for(var i = 0; i < this.word.length; i++){
+            showThis += this.word[i].char;
+        }
+        console.log(showThis);
+    }
 }
 
-var ya = new Word("ya");
-
-console.log(ya.guess);
+module.exports = Word;
