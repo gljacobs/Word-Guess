6116ce6edefeec9ff497b6763b1284917ca025b8
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-25 15:00:37 -0700

    handle reoccuring question prompting

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var inquirer = require("inquirer");
 var Word = require("./word");
 
-inquirer.prompt([
-    {
-        input: "input",
-        message: "\nPlease guess a letter....\n",
-        name: "userGuess"
-    }
-])
-.then((iResponse) => {
-    var input = new Word(iResponse.userGuess);
-    input.display();
-    console.log(input.guess);
-    
-})
+var log = ["tyrannosaurus", "pterodactyl", "velociraptor", "liopleurodon", "brachiosaurus", "triceratops", "stegosaurus"];
+
+var wordToGuess = new Word(log[2]);
+var userIn = false;
+
+//while(wordToGuess.word !== wordToGuess.guess && !userIn) {
+var ask = function() {
+    userIn = true;
+    inquirer.prompt([
+        {
+            input: "input",
+            message: "What letter would you like to guess?",
+            name: "userGuess"
+        }
+    ])
+    .then((iResponse) => {
+        userIn = iResponse.userGuess;
+        userIn = false;
+        while(wordToGuess.word !== wordToGuess.guess && !userIn) {
+            ask();
+        }
+    });    
+}
+
+ask();
+//}
